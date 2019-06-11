2c5f3bcfc91a8a676580108f9339c134514bbe18
Q. Price <priceqp@gmail.com>
2019-06-10 21:50:43 -0500

    Syntax Update

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 //jshint esversion:6
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
-const app = express();
+const bodyParser = require("body-parser"),
+      express = require("express"),
+      request = require("request"),
+      app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
-
 app.post("/", function(req, res){
-  var crypto = req.body.crypto;
-  var fiat = req.body.fiat;
-  var amount = req.body.amount;
-  var options = {
-    url: "https://apiv2.bitcoinaverage.com/convert/global",
-    method: "GET",
-    qs: {
-      from: crypto,
-      to: fiat,
-      amount: amount
-    }
-  };
+  let crypto = req.body.crypto,
+      fiat = req.body.fiat,
+      amount = req.body.amount,
+      options = {
+        url: "https://apiv2.bitcoinaverage.com/convert/global",
+        method: "GET",
+        qs: {
+          from: crypto,
+          to: fiat,
+          amount: amount
+        }
+      };
   request(options, function(error, response, body){
-    var data = JSON.parse(body);
-    var price = data.price;
-    var currentDate = data.time;
-
+    let data = JSON.parse(body),
+        price = data.price,
+        currentDate = data.time;
     res.write("<p>The current date is " + currentDate + "</p>");
     res.write("<h1>" + amount + " " + crypto + " is currently worth " + price + " " + fiat + "</h1>");
     res.send();
